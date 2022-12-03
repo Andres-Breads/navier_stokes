6e7009ddc08b933015079ebc48da8c16e8ad7362
Carlos Aucaruri <programador@laborfinanciera.com>
2022-12-02 23:10:28 -0500

    graph added with our matrix

--- gaus_seidel_method.py
+++ gaus_seidel_method.py
@@ -62,10 +62,25 @@ if __name__ == "__main__":
     B = np.loadtxt('matriz_X_B.txt', skiprows=0)
     C = np.loadtxt('matriz_Y_A.txt', skiprows=0)
     D = np.loadtxt('matriz_Y_B.txt', skiprows=0)
-    solucionX = resolver_gaus_seidel(A, B)
-    solucionY = resolver_gaus_seidel(C, D)
-    ''' solucion = np.meshgrid(solucionX, solucionY)
-    print(solucion)
-    plt.imshow( solucion , origin='lower' )
+    X = resolver_gaus_seidel(A, B)
+    Y = resolver_gaus_seidel(C, D)
+
+    matriz_n = 8 # Filas
+    matriz_m = 11 # columnas
+
+    Solucion = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, X[0], X[1], X[2], X[3], X[4], X[5], 0, 0, 0, 0],
+        [1, X[6], X[7], X[8], X[9], X[10], X[11], 0, 0, 0, 0],
+        [1, X[12], X[13], X[14], X[15], X[16], X[17], X[18], X[19], X[20], 0],
+        [1, X[21], X[22], X[23], 0, 0, 0, X[24], X[25], X[26], 0],
+        [1, X[27], X[28], X[29], 0, 0, 0, X[30], X[31], X[32], 0],
+        [1, X[33], X[34], X[35], 0, 0, 0, X[36], X[37], X[38], 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    #plt.figure()
+    plt.imshow(Solucion)
     plt.title( "2-D Heat Map" )
-    plt.show() '''
+    plt.colorbar()
+    plt.show()
--- newton_raphson_method.py
+++ newton_raphson_method.py
@@ -8,13 +8,22 @@ x0 = 2 # Punto inicial
 tolera = 0.0001
 
 # PROCEDIMIENTO
-xi = x0
+tabla = []
 tramo = abs(2*tolera)
+xi = x0
 while not(tramo <= tolera):
     xnuevo = xi - fx(xi)/dfx(xi)
     tramo = abs(xnuevo - xi) # Error estimado
+    tabla.append([xi,xnuevo,tramo])
     xi = xnuevo
 
+# convierte la lista a un arreglo.
+tabla = np.array(tabla)
+n = len(tabla)
+
 # SALIDA
-print(xnuevo)
-print(tramo)
+print(['xi', 'xnuevo', 'tramo'])
+np.set_printoptions(precision = 4)
+print(tabla)
+print('raiz en: ', xi)
+print('con error de: ',tramo)
